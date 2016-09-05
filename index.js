b0f764911dae2a56f4c0004482cfd032f04687f8
Peter Evers <pevers90@gmail.com>
2016-09-05 22:41:55 +0200

    FIXED LITTLE THINGY

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 var oldConsoleLog = console.log;
 console.log = function() {
 	oldConsoleLog.apply(this, Object.keys(arguments)
-		.filter(key => typeof arguments[key] === 'string')
-		.map(key => arguments[key].toUpperCase()));
+		.map(key => typeof arguments[key] === 'string' ? arguments[key].toUpperCase() : arguments[key]));
 };
